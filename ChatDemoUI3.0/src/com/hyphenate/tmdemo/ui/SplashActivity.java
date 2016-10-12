cc8a397e57470b3f92748e1d87a1c8c59ec7ac7b
ralphgult <ralph.gult@gmail.com>
2016-10-13 01:11:21 +0800

    添加Activity RalphGult 2016-10-10

--- ChatDemoUI3.0/src/com/hyphenate/tmdemo/ui/SplashActivity.java
+++ ChatDemoUI3.0/src/com/hyphenate/tmdemo/ui/SplashActivity.java
@@ -9,70 +9,70 @@ import com.hyphenate.chat.EMClient;
 import com.hyphenate.tmdemo.DemoHelper;
 import com.hyphenate.tmdemo.R;
 
+import tm.ui.welcome.GuideActivity;
+
 /**
  * 开屏页
- *
  */
 public class SplashActivity extends BaseActivity {
-	private RelativeLayout rootLayout;
-	private TextView versionText;
-	
-	private static final int sleepTime = 2000;
+    private RelativeLayout rootLayout;
+    private TextView versionText;
 
-	@Override
-	protected void onCreate(Bundle arg0) {
-		setContentView(R.layout.em_activity_splash);
-		super.onCreate(arg0);
+    private static final int sleepTime = 2000;
 
-		rootLayout = (RelativeLayout) findViewById(R.id.splash_root);
-		versionText = (TextView) findViewById(R.id.tv_version);
+    @Override
+    protected void onCreate(Bundle arg0) {
+        setContentView(R.layout.em_activity_splash);
+        super.onCreate(arg0);
+
+        rootLayout = (RelativeLayout) findViewById(R.id.splash_root);
+        versionText = (TextView) findViewById(R.id.tv_version);
 
 //		versionText.setText(getVersion());
 //		AlphaAnimation animation = new AlphaAnimation(0.3f, 1.0f);
 //		animation.setDuration(1500);
 //		rootLayout.startAnimation(animation);
-	}
+    }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-		new Thread(new Runnable() {
-			public void run() {
-				if (DemoHelper.getInstance().isLoggedIn()) {
-					// auto login mode, make sure all group and conversation is loaed before enter the main screen
-					long start = System.currentTimeMillis();
-					EMClient.getInstance().groupManager().loadAllGroups();
-					EMClient.getInstance().chatManager().loadAllConversations();
-					long costTime = System.currentTimeMillis() - start;
-					//wait
-					if (sleepTime - costTime > 0) {
-						try {
-							Thread.sleep(sleepTime - costTime);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-					//enter main screen
-					startActivity(new Intent(SplashActivity.this, MainActivity.class));
-					finish();
-				}else {
-					try {
-						Thread.sleep(sleepTime);
-					} catch (InterruptedException e) {
-					}
-					startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-					finish();
-				}
-			}
-		}).start();
+        new Thread(new Runnable() {
+            public void run() {
 
-	}
-	
-	/**
-	 * get sdk version
-	 */
-	private String getVersion() {
-	    return EMClient.getInstance().getChatConfig().getVersion();
-	}
+                try {
+                    if (DemoHelper.getInstance().isLoggedIn()) {
+                        // auto login mode, make sure all group and conversation is loaed before enter the main screen
+                        long start = System.currentTimeMillis();
+                        EMClient.getInstance().groupManager().loadAllGroups();
+                        EMClient.getInstance().chatManager().loadAllConversations();
+                        long costTime = System.currentTimeMillis() - start;
+                        //wait
+                        if (sleepTime - costTime > 0) {
+                            Thread.sleep(sleepTime - costTime);
+                        }
+                        //enter main screen
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                        finish();
+                    } else {
+                        Thread.sleep(sleepTime);
+                        startActivity(new Intent(SplashActivity.this, GuideActivity.class));
+
+                        finish();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    /**
+     * get sdk version
+     */
+    private String getVersion() {
+        return EMClient.getInstance().getChatConfig().getVersion();
+    }
 }
