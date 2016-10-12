3526e4b3ec6e7e16ced66045eb46e2209ff27615
ralphgult <ralph.gult@gmail.com>
2016-10-13 01:09:23 +0800

    添加ConfirmDialog RalphGult 2016-10-12

--- ChatDemoUI3.0/src/tm/utils/dialog/DialogFactory.java
+++ ChatDemoUI3.0/src/tm/utils/dialog/DialogFactory.java
@@ -6,15 +6,20 @@ import com.hyphenate.tmdemo.R;
 
 
 public class DialogFactory {
-	public static final int DIALOG_TYPE_INPUT = 2;
-	public static PartentDialog createDialog(Context context, int type) {
-		PartentDialog dialog = null;
-		switch (type) {
-		case DIALOG_TYPE_INPUT:
-			dialog = new InputDialog(context,
-					R.layout.yx_common_input_dialog_layout);
-			break;
-		}
-		return dialog;
-	}
+    public static final int DIALOG_TYPE_INPUT = 2;
+    public static final int DIALOG_TYPE_CONFIRM = 4;
+
+    public static PartentDialog createDialog(Context context, int type) {
+        PartentDialog dialog = null;
+        switch (type) {
+            case DIALOG_TYPE_INPUT:
+                dialog = new InputDialog(context,
+                        R.layout.yx_common_input_dialog_layout);
+                break;
+            case DIALOG_TYPE_CONFIRM:
+                dialog = new ConfirmDialog(context,R.layout.youxue_aty_confirm_dialog_layout);
+                break;
+        }
+        return dialog;
+    }
 }
