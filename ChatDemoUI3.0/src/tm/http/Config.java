42b03fcc7a84b1cfc3aa199b7a9336fcd917fcd2
ralphgult <ralph.gult@gmail.com>
2016-10-27 23:41:26 +0800

    12.27

--- ChatDemoUI3.0/src/tm/http/Config.java
+++ ChatDemoUI3.0/src/tm/http/Config.java
@@ -17,6 +17,7 @@ import android.os.Environment;
 public class Config {
 //    public static String ROOT_HOST_NAME="http://hsaiqs.xicp.net:20806";
     public static String ROOT_HOST_NAME="http://121.196.244.27:3003";
+//        public static String ROOT_HOST_NAME="http://192.168.1.110:8080";
 
     public static String URL_LOGIN_USER=ROOT_HOST_NAME+"/CBDParkingMSs/login"; // 用户登录
     public static String URL_GET_RECOMMEND_USERS=ROOT_HOST_NAME+"/CBDParkingMSs/recommend";//获取推荐列表接口
@@ -24,10 +25,11 @@ public class Config {
     public static String URL_GET_TS_USERS=ROOT_HOST_NAME+"/CBDParkingMSs/ts";//获取本地特色列表接口
     public static String URL_GET_USER_HOME=ROOT_HOST_NAME+"/CBDParkingMSs/mood/personal";//获取个人首页接口
     public static String URL_GET_QIYE_HOME=ROOT_HOST_NAME+"/CBDParkingMSs/goods/homepage";//获取企业首页接口
-    public static String URL_GET_SANNONG_HOME=ROOT_HOST_NAME+"/CBDParkingMSs/mood/farmPersonal";//获取三农首页接口
     public static String URL_GET_SEARCH_HOME=ROOT_HOST_NAME+"/CBDParkingMSs/searchRecommend";//获取热荐搜索接口
+    public static String URL_GET_ADDFRIEND=ROOT_HOST_NAME+"/CBDParkingMSs/friends/addFriend";//添加好友接口
     public static String URL_REDGIST = ROOT_HOST_NAME+"/CBDParkingMSs/regist";
     public static String URL_FIND_PASSWORD = ROOT_HOST_NAME+"/CBDParkingMSs/updatePwd";
+
 
     public static void init(Context context) throws IOException {
     }
