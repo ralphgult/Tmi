1a36de99f00c1492c05eff1fdda17e1c94eee29c
ralphgult <ralph.gult@gmail.com>
2016-11-09 05:22:24 +0800

    修改 高培 2016-11-08

--- ChatDemoUI3.0/src/tm/manager/PersonManager.java
+++ ChatDemoUI3.0/src/tm/manager/PersonManager.java
@@ -2,26 +2,47 @@ package tm.manager;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Base64;
 import android.util.Log;
 
 import com.hyphenate.chat.EMClient;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import internal.org.apache.http.entity.mime.MultipartEntity;
+import internal.org.apache.http.entity.mime.content.ContentBody;
+import internal.org.apache.http.entity.mime.content.FileBody;
+import internal.org.apache.http.entity.mime.content.StringBody;
+import tm.http.Config;
 
 /**
  * Created by RalphGult on 8/29/2016.
@@ -33,60 +54,59 @@ public class PersonManager {
     public static int TYPE_COMPINTOR = 1;
     public static int TYPE_FARMNAME = 2;
     public static int TYPE_FARMINTOR = 3;
+    public static int UPLOAD_HEADICON_SUCESS = 1001;
+    public static int UPLOAD_HEADICON_ERROR = 1002;
 
 
-    private PersonManager personManager = null;
-    private String url = null;
 
+    /**
+     * 上传头像
+     *
+     * @param file    头像文件
+     * @param userId  用户Id
+     * @param handler handler
+     */
+    public static void SubmitPost(File file, String userId, Handler handler) {
+        HttpClient httpclient = new DefaultHttpClient();
+        try {
+            HttpPost httppost = new HttpPost(Config.URL_UPDATE_IMAGE);
+            FileBody bin = new FileBody(file);
+            MultipartEntity reqEntity = new MultipartEntity();
+            reqEntity.addPart("file", bin);//file1为请求后台的File upload;属性
+            reqEntity.addPart("userId", new StringBody(userId));
+            httppost.setEntity(reqEntity);
+            HttpResponse response = httpclient.execute(httppost);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode == HttpStatus.SC_OK) {
+                System.out.println("服务器正常响应.....");
+                HttpEntity resEntity = response.getEntity();
+                JSONObject object = new JSONObject(EntityUtils.toString(resEntity));//httpclient自带的工具类读取返回数据
 
-    public void uploadPersonHeanPic(File file) throws IOException {
-        String userId = EMClient.getInstance().getCurrentUser();
-        String fileString = null;
-        url  = "http://hsaiqs.xicp.net:20806/CBDParkingMSs/";
-        //文件转换为String
-        file = new File("/storage/emulated/0/RM/res/icon/role/1006_head.png");
-        Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        byte[] appicon = baos.toByteArray();
-        fileString = Base64.encodeToString(appicon,Base64.DEFAULT);
-
-        if(null != fileString){
-            //开始请求
-            HttpPost httpPost = new HttpPost(url + "updatePhoto");
-
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("file", fileString));
-            params.add(new BasicNameValuePair("userId", userId));
-
-            HttpResponse httpResponse = null;
-            try {
-                httpPost.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
-                httpResponse = new DefaultHttpClient().execute(httpPost);
-                if (httpResponse.getStatusLine().getStatusCode() == 200) {
-                    String result = EntityUtils.toString(httpResponse.getEntity());
-                    if(null != result){
-                        Log.e("info","result ======= " + result);
-                        JSONObject object = new JSONObject(result);
-                        if(object.getInt("authId") == 0){
-                            //上传成功
-                        }else{
-
-                        }
+                if (null != handler) {
+                    if (object.getInt("authId") == 1) {
+                        handler.sendEmptyMessage(UPLOAD_HEADICON_SUCESS);
+                    }else{
+                        handler.sendEmptyMessage(UPLOAD_HEADICON_ERROR);
                     }
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (null != handler) {
+                handler.sendEmptyMessage(UPLOAD_HEADICON_ERROR);
+            }
+        } finally {
+            try {
+                httpclient.getConnectionManager().shutdown();
+            } catch (Exception ignore) {
+            }
         }
     }
 
-    public void updateTexts(String text,int type){
+    public static void updateTexts(String text, int type, Handler handler) {
         String userId = EMClient.getInstance().getCurrentUser();
-        url  = "http://hsaiqs.xicp.net:20806/CBDParkingMSs/";
         String key = null;
-        switch (type){
+        switch (type) {
             case 0:
                 key = "companyName";
                 break;
@@ -100,8 +120,8 @@ public class PersonManager {
                 key = "farmIntroduction";
                 break;
         }
-        if(null != text){
-            HttpPost httpPost = new HttpPost(url + "updatemse");
+        if (null != text) {
+            HttpPost httpPost = new HttpPost(Config.URL_CHANGE_INFO);
             List<NameValuePair> params = new ArrayList<NameValuePair>();
             params.add(new BasicNameValuePair(key, text));
             params.add(new BasicNameValuePair("userId", userId));
@@ -112,18 +132,38 @@ public class PersonManager {
                 httpResponse = new DefaultHttpClient().execute(httpPost);
                 if (httpResponse.getStatusLine().getStatusCode() == 200) {
                     String result = EntityUtils.toString(httpResponse.getEntity());
-                    if(null != result){
-                        Log.e("info","result ======= " + result);
+                    if (null != result) {
+                        Log.e("info", "result ======= " + result);
                         JSONObject object = new JSONObject(result);
-                        if(object.getInt("authId") == 0){
+                        if (object.getInt("authId") == 0) {
                             //上传成功
-                        }else{
-
+                            Log.e("info","change Sucess");
+                            if(null != handler){
+                                Message msg = new Message();
+                                msg.what = 1001;
+                                msg.arg1 = type;
+                                handler.sendMessage(msg);
+                            }
+                        } else {
+                            Log.e("info","change Sucess");
+                            if(null != handler){
+                                Message msg = new Message();
+                                msg.what = 1002;
+                                msg.arg1 = type;
+                                msg.obj = text;
+                                handler.sendMessage(msg);
+                            }
                         }
                     }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
+                if(null != handler){
+                    Message msg = new Message();
+                    msg.what = 1002;
+                    msg.arg1 = type;
+                    handler.sendMessage(msg);
+                }
             }
         }
     }
