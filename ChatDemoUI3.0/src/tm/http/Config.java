ac2cdd5d94a1e605d85caf9442e267784061087c
ralphgult <ralph.gult@gmail.com>
2016-11-12 21:17:28 +0800

    修改 高培 2016-11-12

--- ChatDemoUI3.0/src/tm/http/Config.java
+++ ChatDemoUI3.0/src/tm/http/Config.java
@@ -3,6 +3,7 @@ package tm.http;
 /**
  * Created by Administrator on 2016/9/7.
  */
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,27 +16,30 @@ import android.content.res.Configuration;
 import android.os.Environment;
 
 public class Config {
-//    public static String ROOT_HOST_NAME="http://hsaiqs.xicp.net:20806";
-    public static String ROOT_HOST_NAME="http://121.196.244.27:3003";
+    //    public static String ROOT_HOST_NAME="http://hsaiqs.xicp.net:20806";
+//    public static String ROOT_HOST_NAME="http://121.196.244.27:3003";
+    public static String ROOT_HOST_NAME = "http://192.168.1.101:8080";
 //        public static String ROOT_HOST_NAME="http://192.168.1.110:8080";
 
-    public static String URL_LOGIN_USER=ROOT_HOST_NAME+"/CBDParkingMSs/login"; // 用户登录
-    public static String URL_GET_RECOMMEND_USERS=ROOT_HOST_NAME+"/CBDParkingMSs/recommend";//获取推荐列表接口
-    public static String URL_GET_NEARLY_USERS=ROOT_HOST_NAME+"/CBDParkingMSs/near";//获取附近列表接口
-    public static String URL_GET_TS_USERS=ROOT_HOST_NAME+"/CBDParkingMSs/ts";//获取本地特色列表接口
-    public static String URL_GET_USER_HOME=ROOT_HOST_NAME+"/CBDParkingMSs/mood/personal";//获取个人首页接口
-    public static String URL_GET_QIYE_HOME=ROOT_HOST_NAME+"/CBDParkingMSs/goods/homepage";//获取企业首页接口
-    public static String URL_GET_SEARCH_HOME=ROOT_HOST_NAME+"/CBDParkingMSs/searchRecommend";//获取热荐搜索接口
-    public static String URL_GET_ADDFRIEND=ROOT_HOST_NAME+"/CBDParkingMSs/friends/addFriend";//添加好友接口
-    public static String URL_GET_ZIXUN_HOME=ROOT_HOST_NAME+"/CBDParkingMSs/information";//添加主页资讯接口
-    public static String URL_MOMENT = ROOT_HOST_NAME+"/CBDParkingMSs/mood/pyq";//朋友圈接口
-    public static String URL_REDGIST = ROOT_HOST_NAME+"/CBDParkingMSs/regist";//注册接口
-    public static String URL_FIND_PASSWORD = ROOT_HOST_NAME+"/CBDParkingMSs/updatePwd";//修改密码接口
+    public static String URL_LOGIN_USER = ROOT_HOST_NAME + "/CBDParkingMSs/login"; // 用户登录
+    public static String URL_GET_RECOMMEND_USERS = ROOT_HOST_NAME + "/CBDParkingMSs/recommend";//获取推荐列表接口
+    public static String URL_GET_NEARLY_USERS = ROOT_HOST_NAME + "/CBDParkingMSs/near";//获取附近列表接口
+    public static String URL_GET_TS_USERS = ROOT_HOST_NAME + "/CBDParkingMSs/ts";//获取本地特色列表接口
+    public static String URL_GET_USER_HOME = ROOT_HOST_NAME + "/CBDParkingMSs/mood/personal";//获取个人首页接口
+    public static String URL_GET_QIYE_HOME = ROOT_HOST_NAME + "/CBDParkingMSs/goods/homepage";//获取企业首页接口
+    public static String URL_GET_SEARCH_HOME = ROOT_HOST_NAME + "/CBDParkingMSs/searchRecommend";//获取热荐搜索接口
+    public static String URL_GET_ADDFRIEND = ROOT_HOST_NAME + "/CBDParkingMSs/friends/addFriend";//添加好友接口
+    public static String URL_GET_ZIXUN_HOME = ROOT_HOST_NAME + "/CBDParkingMSs/information";//添加主页资讯接口
+    public static String URL_MOMENT = ROOT_HOST_NAME + "/CBDParkingMSs/mood/pyq";//朋友圈接口
+    public static String URL_REDGIST = ROOT_HOST_NAME + "/CBDParkingMSs/regist";//注册接口
+    public static String URL_FIND_PASSWORD = ROOT_HOST_NAME + "/CBDParkingMSs/updatePwd";//修改密码接口
     public static String URL_GET_USER_POFILE = ROOT_HOST_NAME + "/CBDParkingMSs/info";//我的页面获取用户信息接口
     public static String URL_GET_USRE_FACEWALL = ROOT_HOST_NAME + "/CBDParkingMSs/faceScore/faceScore";//获取用户颜值表
     public static String URL_CHANGE_SIGN = ROOT_HOST_NAME + "/CBDParkingMSs/updatemse";//修改信息
     public static String URL_UPDATE_IMAGE = ROOT_HOST_NAME + "/CBDParkingMSs/updatePhoto";//上传头像
     public static String URL_CHANGE_INFO = ROOT_HOST_NAME + "/CBDParkingMSs/updatemse";//修改信息
+    public static String URL_GET_TMIMESSAGE = ROOT_HOST_NAME + "/CBDParkingMSs/mood/information";//获取T觅资讯列表
+    public static String URL_ADD_MOMENT = ROOT_HOST_NAME + "/CBDParkingMSs/mood/addMood";//发布朋友圈
 
 
     public static void init(Context context) throws IOException {
