7ef07b8f54c09ef3199a23b4741059c0e0e6733d
Lking <1599863094@qq.com>
2016-12-02 00:16:12 +0800

    定位 刘建斌 2016.12.1

--- ChatDemoUI3.0/src/com/xbh/tmi/ui/SplashActivity.java
+++ ChatDemoUI3.0/src/com/xbh/tmi/ui/SplashActivity.java
@@ -1,10 +1,16 @@
 package com.xbh.tmi.ui;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.hyphenate.chat.EMClient;
 import com.xbh.tmi.R;
 import com.xbh.tmi.DemoHelper;
@@ -22,9 +28,21 @@ public class SplashActivity extends BaseActivity {
     private boolean hasOpened;
 
     private static final int sleepTime = 2000;
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+    static BDLocation lastLocation = null;
 
     @Override
     protected void onCreate(Bundle arg0) {
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener( myListener );    //注册监听函数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);// open gps
+        option.setCoorType("gcj02");
+        option.setScanSpan(30000);
+        option.setAddrType("all");
+        mLocationClient.setLocOption(option);
+//        mLocationClient.start();
         setContentView(R.layout.em_activity_splash);
         super.onCreate(arg0);
         //获取SharedPreferences对象
@@ -83,5 +101,55 @@ public class SplashActivity extends BaseActivity {
      */
     private String getVersion() {
         return EMClient.getInstance().getChatConfig().getVersion();
+    }
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (location == null) {
+                return;
+            }
+            lastLocation = location;
+            Log.e("info","lat=111111122221111111111="+lastLocation.getLatitude());
+            Log.e("info","lng=11111111122222211111111="+lastLocation.getLongitude());
+            saveLoginInfo(SplashActivity.this,lastLocation.getLatitude(),lastLocation.getLongitude());
+
+        }
+    }
+    /**
+     * 使用SharedPreferences保存用户登录信息
+     * @param context
+     */
+    public static void saveLoginInfo(Context context, double lat , double lng){
+        //获取SharedPreferences对象
+        SharedPreferences sharedPre=context.getSharedPreferences("config", context.MODE_PRIVATE);
+        //获取Editor对象
+        SharedPreferences.Editor editor=sharedPre.edit();
+        //设置参数
+        editor.putString("lat", String.valueOf(lat));
+        editor.putString("lng", String.valueOf(lng));
+        //提交
+        editor.commit();
+    }
+    @Override
+    protected void onDestroy() {
+        if (mLocationClient != null)
+            mLocationClient.stop();
+        super.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        if (mLocationClient != null) {
+            mLocationClient.start();
+        }
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        if (mLocationClient != null) {
+            mLocationClient.stop();
+        }
+        super.onPause();
+        lastLocation = null;
     }
 }
