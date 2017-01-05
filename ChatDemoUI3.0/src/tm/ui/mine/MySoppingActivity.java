5080ba507d4c430e1706317d635e089a56fd83b7
RG <ralph.gult@gmail.com>
2017-01-05 10:17:26 +0800

    修改 2017-01-05

--- ChatDemoUI3.0/src/tm/ui/mine/MySoppingActivity.java
+++ ChatDemoUI3.0/src/tm/ui/mine/MySoppingActivity.java
@@ -2,14 +2,26 @@ package tm.ui.mine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.xbh.tmi.R;
 
 public class MySoppingActivity extends Activity {
-
+    private ImageView mBack_iv;
+    private TextView mTotalPri_tv;
+    private Button mBuy_tv;
+    private ListView mList_lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_sopping);
+    }
+    private void initView(){
+        mBack_iv = (ImageView) findViewById(R.id.my_shopping_back_iv);
+        mTotalPri_tv = (TextView) findViewById(R.id.xy_shopping_total_price_tv);
+        mBuy_tv = (Button) findViewById(R.id.my_shopping_pay_btn);
     }
 }
