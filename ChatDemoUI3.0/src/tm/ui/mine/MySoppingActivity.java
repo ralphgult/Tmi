a9ace1f3b33617cee06d3fb6814b9e659a32ea55
RalphGult <ralph.gult@gmail.com>
2017-01-08 22:45:41 +0800

    完善购物车界面 2017-01-05

--- ChatDemoUI3.0/src/tm/ui/mine/MySoppingActivity.java
+++ ChatDemoUI3.0/src/tm/ui/mine/MySoppingActivity.java
@@ -2,26 +2,183 @@ package tm.ui.mine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.ArrayMap;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.plus.model.people.Person;
 import com.xbh.tmi.R;
 
-public class MySoppingActivity extends Activity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import tm.manager.PersonManager;
+import tm.ui.mine.adapter.MyShoppingAdapter;
+import tm.ui.tmi.GoodsDetilActivity;
+import tm.utils.ViewUtil;
+
+public class MySoppingActivity extends Activity implements View.OnClickListener {
     private ImageView mBack_iv;
     private TextView mTotalPri_tv;
     private Button mBuy_tv;
+    private TextView mEdit_tv;
     private ListView mList_lv;
+
+    private List<Map<String, String>> mDataList;
+    private MyShoppingAdapter mAdapter;
+    public Handler mHandler;
+    private boolean misEdit;
+    private float mTotalPrice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_sopping);
+        init();
+        initView();
+        getShoppingList();
     }
-    private void initView(){
+
+    private void init() {
+        mDataList = new ArrayList<>();
+        mAdapter = new MyShoppingAdapter(this, false);
+        misEdit = false;
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case 1001:
+                        try {
+                            JSONArray array = (JSONArray) msg.obj;
+                            if (null != array && array.length() > 0) {
+                                int size = array.length();
+                                mDataList = new ArrayList<>(size);
+                                Map<String, String> map = null;
+                                for (int i = 0; i < size; i++) {
+                                    JSONObject object = array.getJSONObject(i);
+                                    map = new HashMap<>();
+                                    map.put("goodId", object.getString("goodId"));
+                                    map.put("goodName", object.getString("goodName"));
+                                    map.put("scId", object.getString("scId"));
+                                    map.put("currentPrice", object.getString("currentPrice"));
+                                    map.put("num", object.getString("num"));
+                                    map.put("goodImg", object.getString("goodImg"));
+                                    map.put("createDate", object.getString("createDate"));
+                                    mDataList.add(map);
+                                }
+                                mAdapter.resetData(mDataList);
+                                mAdapter.setIsEdit(misEdit);
+                                mList_lv.setAdapter(mAdapter);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case 2001:
+                        Toast.makeText(MySoppingActivity.this, "商品已删除", Toast.LENGTH_SHORT).show();
+                        mDataList.remove((Map<String, String>) msg.obj);
+                        mAdapter.resetData(mDataList);
+                        break;
+                    default:
+                        Toast.makeText(MySoppingActivity.this, "系统繁忙，请稍后再试...", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+    }
+
+    private void initView() {
         mBack_iv = (ImageView) findViewById(R.id.my_shopping_back_iv);
         mTotalPri_tv = (TextView) findViewById(R.id.xy_shopping_total_price_tv);
         mBuy_tv = (Button) findViewById(R.id.my_shopping_pay_btn);
+        mList_lv = (ListView) findViewById(R.id.my_shopping_list_lv);
+        mEdit_tv = (TextView) findViewById(R.id.my_shopping_edit_tv);
+        mBack_iv.setOnClickListener(this);
+        mBuy_tv.setOnClickListener(this);
+        mEdit_tv.setOnClickListener(this);
+        mList_lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Bundle bundle = new Bundle();
+                bundle.putString("id", mDataList.get(position).get("goodId"));
+                ViewUtil.jumpToOtherActivity(MySoppingActivity.this, GoodsDetilActivity.class, bundle);
+            }
+        });
+    }
+
+    private void getShoppingList() {
+        new Thread() {
+            @Override
+            public void run() {
+                PersonManager.getShoppingList(mHandler);
+            }
+        }.start();
+    }
+
+    public void setTotalPrice() {
+        mTotalPrice = 0.00f;
+        List<String> idList = mAdapter.getmSeletedIds();
+//        for (Map<String, String> map : mDataList) {
+//            if (idList.contains(map.get("scId"))) {
+//                float price = Float.valueOf(map.get("currentPrice")) * Float.valueOf(map.get("count") + ".00");
+//                mTotalPrice = mTotalPrice + price;
+//            }
+//        }
+        mTotalPri_tv.setText("￥" + String.valueOf(mTotalPrice));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.my_shopping_back_iv:
+                ViewUtil.backToOtherActivity(this);
+                break;
+            case R.id.my_shopping_edit_tv:
+                if (misEdit) {
+                    mEdit_tv.setText("编辑");
+                } else {
+                    mEdit_tv.setText("完成");
+                }
+                misEdit = !misEdit;
+                mAdapter.setIsEdit(misEdit);
+                break;
+            case R.id.my_shopping_pay_btn:
+                List<String> ids = mAdapter.getmSeletedIds();
+                ArrayList<String> mNameList = new ArrayList<>();
+                ArrayList<String> mImgList = new ArrayList<>();
+                ArrayList<String> mPriList = new ArrayList<>();
+                ArrayList<String> mCountList = new ArrayList<>();
+                ArrayList<String> mGoodIdList = new ArrayList<>();
+                for (Map<String, String> map : mDataList) {
+                    if (ids.contains(map.get("scId"))) {
+                        mNameList.add(map.get("goodName"));
+                        mImgList.add(map.get("goodImg"));
+                        mPriList.add(map.get("currentPrice"));
+                        mCountList.add(map.get("num"));
+                        mGoodIdList.add(map.get("goodId"));
+                    }
+                }
+                Bundle bundle = new Bundle();
+                bundle.putStringArrayList("names", mNameList);
+                bundle.putStringArrayList("imgs", mImgList);
+                bundle.putStringArrayList("pris", mPriList);
+                bundle.putStringArrayList("counts", mCountList);
+                bundle.putStringArrayList("ids", mGoodIdList);
+                ViewUtil.jumpToOtherActivity(this, ShoppingPayActivity.class, bundle);
+                break;
+        }
     }
 }
--- ChatDemoUI3.0/src/tm/ui/mine/adapter/MyShoppingAdapter.java
+++ ChatDemoUI3.0/src/tm/ui/mine/adapter/MyShoppingAdapter.java
@@ -1,12 +1,26 @@
 package tm.ui.mine.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.xbh.tmi.R;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import tm.manager.PersonManager;
+import tm.ui.mine.MySoppingActivity;
+import tm.ui.mine.ShoppingPayActivity;
+import tm.utils.ImageLoaders;
 
 /**
  * Created by RG on 2017/1/4.
@@ -15,28 +29,150 @@ import java.util.Map;
 public class MyShoppingAdapter extends BaseAdapter {
     private List<Map<String, String>> mDatas;
     private Context mContext;
+    private ViewHolder vh;
+    private ImageLoaders imageLoaders;
+    private List<String> mSeletedIds;
+    private boolean mIsEdit;
+    private boolean mIsShopping;
 
-    public MyShoppingAdapter(Context context){
-        mContext  = context;
+    public MyShoppingAdapter(Context context, boolean isShopping) {
+        mContext = context;
+        imageLoaders = new ImageLoaders(mContext, new imageLoaderListener());
+        mSeletedIds = new ArrayList<>();
+        mIsEdit = false;
+        mIsShopping = isShopping;
+    }
+
+    /**
+     * map.put("goodId", object.getString("goodId"));
+     * map.put("goodName",object.getString("goodName"));
+     * map.put("scId",object.getString("scId"));
+     * map.put("currentPrice",object.getString("currentPrice"));
+     * map.put("num",object.getString("num"));
+     * map.put("goodImg",object.getString("goodImg"));
+     * map.put("createDate",object.getString("createDate"));
+     *
+     * @param datas
+     */
+    public void resetData(List<Map<String, String>> datas) {
+        mDatas = datas;
+        notifyDataSetChanged();
+    }
+
+    public void setIsEdit(boolean isEdit) {
+        mIsEdit = isEdit;
+        resetData(mDatas);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return mDatas.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mDatas.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view;
+        if (convertView == null) {
+            vh = new ViewHolder();
+            view = View.inflate(mContext, R.layout.shpping_item, null);
+            vh.mCheck = (ImageView) view.findViewById(R.id.item_shopping_check_iv);
+            vh.mImage = (ImageView) view.findViewById(R.id.item_shopping_img_iv);
+            vh.mName = (TextView) view.findViewById(R.id.item_shopping_goodsname_tv);
+            vh.mPrice = (TextView) view.findViewById(R.id.item_shopping_price_tv);
+            vh.mCount = (TextView) view.findViewById(R.id.item_shopping_count_tv);
+            vh.mCountLayout = (LinearLayout) view.findViewById(R.id.item_shopping_count_ly);
+            vh.mCountEdit = (EditText) view.findViewById(R.id.item_shopping_count_edt);
+            vh.mDelete = (TextView) view.findViewById(R.id.item_shopping_del_tv);
+            view.setTag(vh);
+        } else {
+            view = convertView;
+            vh = (ViewHolder) view.getTag();
+        }
+        vh.mName.setText(mDatas.get(position).get("goodName"));
+        vh.mPrice.setText(mDatas.get(position).get("currentPrice"));
+        vh.mCount.setText("x" + mDatas.get(position).get("num"));
+        imageLoaders.loadImage(vh.mImage, mDatas.get(position).get("goodImg"));
+        if (mIsShopping) {
+            vh.mCheck.setVisibility(View.GONE);
+            vh.mCountLayout.setVisibility(View.VISIBLE);
+            vh.mCount.setVisibility(View.GONE);
+            vh.mCountEdit.setHint(mDatas.get(position).get("num"));
+            vh.mCountEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (!hasFocus) {
+                        ((ShoppingPayActivity) mContext).setTotalPrice(position,vh.mCountEdit.getText().toString().trim());
+                    }
+                }
+            });
+        } else {
+            vh.mCheck.setVisibility(View.VISIBLE);
+            vh.mCountLayout.setVisibility(View.GONE);
+            vh.mCount.setVisibility(View.VISIBLE);
+            if (mIsEdit) {
+                vh.mDelete.setVisibility(View.VISIBLE);
+                vh.mDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //TODO 删除购物车
+                        new Thread() {
+                            @Override
+                            public void run() {
+                                PersonManager.delShoppingList(mDatas.get(position), ((MySoppingActivity) mContext).mHandler);
+                            }
+                        }.start();
+                    }
+                });
+            } else {
+                vh.mDelete.setVisibility(View.GONE);
+            }
+            vh.mCheck.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (vh.mCheck.isSelected()) {
+                        mSeletedIds.remove(mDatas.get(position).get("scId"));
+                        vh.mCheck.setSelected(false);
+                    } else {
+                        mSeletedIds.add(mDatas.get(position).get("scId"));
+                        vh.mCheck.setSelected(true);
+                    }
+                    ((MySoppingActivity) mContext).setTotalPrice();
+                }
+            });
+        }
+        return view;
+    }
+
+    class ViewHolder {
+        private ImageView mCheck;
+        private ImageView mImage;
+        private TextView mName;
+        private TextView mPrice;
+        private TextView mCount;
+        private LinearLayout mCountLayout;
+        private EditText mCountEdit;
+        private TextView mDelete;
+    }
+
+    private class imageLoaderListener implements ImageLoaders.ImageLoaderListener {
+
+        @Override
+        public void onImageLoad(View v, Bitmap bmp, String url) {
+            ((ImageView) v).setImageBitmap(bmp);
+        }
+    }
+
+    public List<String> getmSeletedIds() {
+        return mSeletedIds;
     }
 }
