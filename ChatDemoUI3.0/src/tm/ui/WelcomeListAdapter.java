b3938712566538d5d001f406b517b643d925507c
messi303 <413039231@qq.com>
2017-01-10 15:52:53 +0800

    视频播放 1.10 赵梦宇

--- ChatDemoUI3.0/src/tm/ui/WelcomeListAdapter.java
+++ ChatDemoUI3.0/src/tm/ui/WelcomeListAdapter.java
@@ -9,7 +9,6 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Handler;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
