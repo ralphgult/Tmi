a4011c6099c1e56d93cd54b57028ecf3f48e4a97
RalphGult <ralph.gult@gmail.com>
2017-03-08 01:03:34 +0800

    修改支付部分代码 2017-03-08

--- ChatDemoUI3.0/src/tm/alipay/AlipayAPI.java
+++ ChatDemoUI3.0/src/tm/alipay/AlipayAPI.java
@@ -1,5 +1,9 @@
 package tm.alipay;
 
+import android.app.Activity;
+
+import com.alipay.sdk.app.PayTask;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
@@ -7,9 +11,7 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Random;
 
-import android.app.Activity;
-
-import com.alipay.sdk.app.PayTask;
+import tm.utils.SysUtils;
 
 public class AlipayAPI {
 
@@ -50,48 +52,60 @@ public class AlipayAPI {
 	 */
 	private static String getOrderInfo(String subject, String body, String price) {
 
-		// 签约合作者身份ID
-		String orderInfo = "partner=" + "\"" + AlipayConfig.PARTNER  + "\"";
+//		// 签约合作者身份ID
+//		String orderInfo = "partner=" + "\"" + AlipayConfig.PARTNER  + "\"";
+//		// 签约卖家支付宝账号
+//		orderInfo += "&seller_id=" + "\"" + AlipayConfig.SELLER + "\"";
 
-		// 签约卖家支付宝账号
-		orderInfo += "&seller_id=" + "\"" + AlipayConfig.SELLER + "\"";
+		String orderInfo = "&app_id=2017030706101528";
 
-		// 商户网站唯一订单号
-		orderInfo += "&out_trade_no=" + "\"" + getOutTradeNo() + "\"";
-
-		// 商品名称
-		orderInfo += "&subject=" + "\"" + subject + "\"";
-
-		// 商品详情
-		orderInfo += "&body=" + "\"" + body + "\"";
-
-		// 商品金额
-		orderInfo += "&total_fee=" + "\"" + price + "\"";
-
-		// 服务器异步通知页面路径
-		orderInfo += "&notify_url=" + "\"" + "http://notify.msp.hk/notify.htm" + "\"";
-
-		// 服务接口名称， 固定值
-		orderInfo += "&service=\"mobile.securitypay.pay\"";
-
-		// 支付类型， 固定值
-		orderInfo += "&payment_type=\"1\"";
+		orderInfo +="&method=\"alipay.trade.app.pay\"";
 
 		// 参数编码， 固定值
-		orderInfo += "&_input_charset=\"utf-8\"";
+		orderInfo += "&charset=\"utf-8\"";
 
-		// 设置未付款交易的超时时间
-		// 默认30分钟，一旦超时，该笔交易就会自动被关闭。
-		// 取值范围：1m～15d。
-		// m-分钟，h-小时，d-天，1c-当天（无论交易何时创建，都在0点关闭）。
-		// 该参数数值不接受小数点，如1.5h，可转换为90m。
-		orderInfo += "&it_b_pay=\"30m\"";
+		orderInfo +="&timestamp=" + SysUtils.getTimeFormat("yyyy-MM-dd HH:mm:ss", System.currentTimeMillis());
 
-		// extern_token为经过快登授权获取到的alipay_open_id,带上此参数用户将使用授权的账户进行支付
-		// orderInfo += "&extern_token=" + "\"" + extern_token + "\"";
+		orderInfo +="&version=\"1.0\"";
+
+		// 服务器异步通知页面路径
+		orderInfo += "&notify_url=" + "\"" + "http://121.196.244.27:3003/notify.htm" + "\"";
+
+		orderInfo +="&biz_content={" + "out_trade_no=" + getOutTradeNo() + ",subject=" + subject
+				+ ",body=" + body + ",total_amount=" + price
+				+ ",timeout_express=\"30m,product_code=QUICK_MSECURITY_PAY,extend_params={}}" ;
+
+//		// 商户网站唯一订单号
+//		orderInfo += "&out_trade_no=" + "\"" + getOutTradeNo() + "\"";
+//
+//		// 商品名称
+//		orderInfo += "&subject=" + "\"" + subject + "\"";
+//
+//		// 商品详情
+//		orderInfo += "&body=" + "\"" + body + "\"";
+//
+//		// 商品金额
+//		orderInfo += "&total_fee=" + "\"" + price + "\"";
+//
+////		// 服务接口名称， 固定值
+////		orderInfo += "&service=\"mobile.securitypay.pay\"";
+//
+////		// 支付类型， 固定值
+////		orderInfo += "&payment_type=\"1\"";
+//
+//
+//		// 设置未付款交易的超时时间
+//		// 默认30分钟，一旦超时，该笔交易就会自动被关闭。
+//		// 取值范围：1m～15d。
+//		// m-分钟，h-小时，d-天，1c-当天（无论交易何时创建，都在0点关闭）。
+//		// 该参数数值不接受小数点，如1.5h，可转换为90m。
+//		orderInfo += "&timeout_express=\"30m\"";
+
+//		// extern_token为经过快登授权获取到的alipay_open_id,带上此参数用户将使用授权的账户进行支付
+//		 orderInfo += "&extern_token=" + "\"" + extern_token + "\"";
 
 		// 支付宝处理完请求后，当前页面跳转到商户指定页面的路径，可空
-		orderInfo += "&return_url=\"m.alipay.com\"";
+//		orderInfo += "&return_url=\"m.alipay.com\"";
 
 		// 调用银行卡支付，需配置此参数，参与签名， 固定值 （需要签约《无线银行卡快捷支付》才能使用）
 		// orderInfo += "&paymethod=\"expressGateway\"";
@@ -112,7 +126,7 @@ public class AlipayAPI {
 	 * get the sign type we use. 获取签名方式
 	 */
 	private static String getSignType() {
-		return "sign_type=\"RSA\"";
+		return "sign_type=\"RSA2\"";
 	}
 
 	/**
