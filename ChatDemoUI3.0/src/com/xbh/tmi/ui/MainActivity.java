e0f780a9856a0e0953ec5c78619690b44215b527
刘建斌 <1599863094@qq.com>
2017-04-17 12:10:06 +0800

    主界面两次按返回键，退出程序到后台 刘建斌 2017.4.17

--- ChatDemoUI3.0/src/com/xbh/tmi/ui/MainActivity.java
+++ ChatDemoUI3.0/src/com/xbh/tmi/ui/MainActivity.java
@@ -549,14 +549,7 @@ public class MainActivity extends BaseActivity {
 		super.onSaveInstanceState(outState);
 	}
 
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_BACK) {
-			moveTaskToBack(false);
-			return true;
-		}
-		return super.onKeyDown(keyCode, event);
-	}
+
 
 	private android.app.AlertDialog.Builder conflictBuilder;
 	private android.app.AlertDialog.Builder accountRemovedBuilder;
@@ -687,4 +680,23 @@ public class MainActivity extends BaseActivity {
 			@NonNull int[] grantResults) {
 		PermissionsManager.getInstance().notifyPermissionsChange(permissions, grantResults);
 	}
+
+	private long exitTime = 0;
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
+			if((System.currentTimeMillis()-exitTime) > 2000){
+				Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+				exitTime = System.currentTimeMillis();
+			} else {
+				moveTaskToBack(false);
+//				finish();
+//				System.exit(0);
+			}
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
 }
